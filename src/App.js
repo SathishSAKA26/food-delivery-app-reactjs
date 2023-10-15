880456e970e8c4ac0e69e9bd8e2c5a85967dbf66
sathish <2000sathishkumar.d@gmail.com>
2023-10-15 10:24:17 +0530

    add some code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Header } from "./components";
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col">
+    <div className="w-screen h-auto flex flex-col bg-slate-400">
       <Header />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { FaOpencart } from "react-icons/fa";
+
+import Logo from "../assets/img/logo.png";
+import Avatar from "../assets/img/avatar.png";
 
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen p-6 px-16">
+    <header className="fixed z-50 w-screen p-6 px-16">
       {/* Desktop & Tablet */}
+      <div className="hidden md:flex w-full h-full justify-between">
+        {/* !logo section */}
+        <div className="flex items-center  gap-2">
+          <img src={Logo} alt="logo" className="w-8 object-cover" />
+          <p className="text-gray-600 text-2xl font-bold">Food</p>
+        </div>
+
+        {/* Right side container */}
+        <div className="flex items-center gap-6">
+          {/* !!list section */}
+          <ul className="flex items-center gap-8">
+            <li className="font-medium text-gray-400 hover:text-gray-700 duration-100 transition-all ease-in-out cursor-pointer">Home</li>
+            <li className="font-medium text-gray-400 hover:text-gray-700 duration-100 transition-all ease-in-out cursor-pointer">Menu</li>
+            <li className="font-medium text-gray-400 hover:text-gray-700 duration-100 transition-all ease-in-out cursor-pointer">About Us</li>
+            <li className="font-medium text-gray-400 hover:text-gray-700 duration-100 transition-all ease-in-out cursor-pointer">Service</li>
+          </ul>
+
+          {/* !cart section */}
+          <div className="relative flex items-center justify-center">
+            <FaOpencart className="text-3xl cursor-pointer text-blue-800" />
+            <div className="absolute top-2 right-2 w-4 h-4 rounded-full">
+              <p className="text-xs font-bold text-red-500 flex items-center justify-center">3</p>
+            </div>
+          </div>
+
+          {/* Avatar */}
+          <img src={Avatar} alt="Profile" className="w-8 min-w-[35px] h-8 min-h-[35px] shadow-xl rounded-full" />
+        </div>
+      </div>
 
       {/* Mobile */}
-    </div>
+      <div className=""></div>
+    </header>
   )
 }
 
